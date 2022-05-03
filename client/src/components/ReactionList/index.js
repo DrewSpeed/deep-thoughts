dc3f259dc052b81f01f41f0ff336b9d8a532e091
Drew Speed <drewjspeed@gmail.com>
2022-05-03 18:40:07 -0500

    added signup and login functionality, updated profile pages and home page ifloggedin

--- client/src/components/ReactionList/index.js
+++ client/src/components/ReactionList/index.js
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom';
 const ReactionList = ({ reactions }) => {
   return (
     <div className="card mb-3">
-    <div className="card-header">
+      <div className="card-header">
         <span className="text-light">Reactions</span>
-    </div>
-    <div className="card-body">
+      </div>
+      <div className="card-body">
         {reactions &&
-        reactions.map(reaction => (
+          reactions.map(reaction => (
             <p className="pill mb-3" key={reaction._id}>
-            {reaction.reactionBody} {'// '}
-            <Link to={`/profile/${reaction.username}`} style={{ fontWeight: 700 }}>
+              {reaction.reactionBody} //{' '}
+              <Link to={`/profile/${reaction.username}`} style={{ fontWeight: 700 }}>
                 {reaction.username} on {reaction.createdAt}
-            </Link>
+              </Link>
             </p>
-        ))}
-    </div>
+          ))}
+      </div>
     </div>
   );
 };
